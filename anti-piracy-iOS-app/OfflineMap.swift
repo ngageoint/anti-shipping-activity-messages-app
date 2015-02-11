060170c704133ace5e86dbca6428c040563adee6
Travis Baumgart <baumgartt@bit-sys.com>
2015-02-11 16:56:41 -0700

    Adding in a port for offline Map rendering (Objective C to Swift).

---
+++ anti-piracy-iOS-app/OfflineMap.swift
@@ -0,0 +1,9 @@
+//
+//  OfflineMap.swift
+//  anti-piracy-iOS-app
+//
+//  Created by Travis Baumgart on 2/10/15.
+//  Copyright (c) 2015 NGA. All rights reserved.
+//
+
+import Foundation
---
+++ anti-piracy-iOS-app/anti-piracy-iOS-app-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  anti-piracy-iOS-app-Bridging-Header.h
+//  anti-piracy-iOS-app
+//
+//  Created by Travis Baumgart on 2/9/15.
+//  Copyright (c) 2015 NGA. All rights reserved.
+//
+
+#ifndef anti_piracy_iOS_app_anti_piracy_iOS_app_Bridging_Header_h
+#define anti_piracy_iOS_app_anti_piracy_iOS_app_Bridging_Header_h
+
+
+#endif
